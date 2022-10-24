135c981fe3d2e23682725a62dab27ac0136ec448
Eduardo Augusto <99891680+edufrantzk@users.noreply.github.com>
2022-10-23 21:42:16 -0300

    Update Query 2.sql

--- Query 2.sql
+++ Query 2.sql
@@ -6,7 +6,8 @@ FROM payment pay
 WHERE amount > (SELECT AVG(amount) FROM payment)
 ORDER BY amount DESC;
 
-/*Puxando todas transações que tem o mesmo valor da maior transação do cliente 1 */
+/*Puxando todas transações que tem o mesmo valor da maior transação do cliente 1 EXTRACTING ALL THE TRANSACTIONS WHICH HAS THE 
+SAME VALUE AS THE CUSTOMER 1 BIGGEST TRANSACTION */
 SELECT *
 FROM payment
 WHERE amount = (
